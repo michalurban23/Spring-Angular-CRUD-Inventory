2493afd80801f7cee82be4b2ada59acc3cb36584
michalurban23 <michalurban23@gmail.com>
2018-05-23 18:35:41 +0200

    First commit

---
+++ src/main/java/com/michalurban/springangularcrudinventory/SpringAngularCrudInventoryApplication.java
@@ -0,0 +1,12 @@
+package com.michalurban.springangularcrudinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularCrudInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularCrudInventoryApplication.class, args);
+	}
+}
